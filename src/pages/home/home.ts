c952171dcd4b9b99f2cde317c34942eb7c0860c3
Patrick Ly <patrickkimly@gmail.com>
2018-04-19 02:23:42 -0700

    Barcode

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  result: BarcodeScanResult;
 
+  constructor(private barcode: BarcodeScanner, public navCtrl: NavController) {}
+
+  async scanBarcode(){
+    try{
+
+        const options: BarcodeScannerOptions = {
+          prompt: 'Point your camera at a barcode',
+          torchOn: true
+        }
+
+        this.result = await this.barcode.scan(options);
+    
+      } catch(error){
+      console.log(error);
+    }
   }
 
 }
